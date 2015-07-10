545a43164010e0efddfae02ee5f3dfcaf5c70901
Tomasz Dybowski <t.dybowski@gmail.com>
2015-07-10 09:38:21 +0200

    Initial commit
    Spring Boot
    Modified jhipster

---
+++ src/main/java/pl/astrait/tshirtCreator/TshirtCreatorApplication.java
@@ -0,0 +1,12 @@
+package pl.astrait.tshirtCreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TshirtCreatorApplication.class, args);
+    }
+}
